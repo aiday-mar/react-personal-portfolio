d0806133348e36b9aa625878f264d06c37209617
aiday-mar <aidaymarlenkyzy@gmail.com>
2020-04-30 17:15:43 +0200

    Added some links at the top of the portfolio.

--- index.js
+++ index.js
@@ -92,7 +92,10 @@ class App extends Component {
     return [
 
         <h1>Aiday Marlen Kyzy</h1>,
-        <p> On this website you may find the work I have done in software engineering and data science previously. You can browse by category in the menu above, and click on the various projects. My field of work so far spans development of Android Java apps, React, Angular, Ruby and Django Web apps, as well as ASP .NET apps written in C#. In the field of data analysis I have worked with R and Python, as well as the ML.NET libarary part of the ASP .NET Core framework. You can browse the projects I have worked on by clicking on the categories below or the menu above.</p>,
+        <p><strong>Email : aidaymarlenkyzy@gmail.com </strong></p>,
+        <p><strong>LinkedIn : https://www.linkedin.com/in/aiday-marlen-kyzy/ </strong></p>,
+        <p><strong>GitHub : https://github.com/aiday-mar </strong></p>,
+        <p> On this website you may find the work I have done in software engineering and data science previously. My field of work so far spans development of Android Java apps, React, Angular, Ruby and Django Web apps, as well as ASP .NET apps written in C#. In the field of data analysis I have worked with R and Python, as well as the ML.NET library part of the ASP .NET Core framework. You can browse the projects I have worked on by clicking on the categories below or the menu above.</p>,
     ];
   }
 }
