18f733658d87945182260a867c7cfbe64106ec69
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-07-13 17:48:52 +0200

    Can't seem to put picture from caroussel in the middle, will have to find other component

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Filter the necessary imports at the end 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
 import Intro from './Components/Main/Intro';
 import Search from './Components/Main/Search';
 import Slides from './Components/Main/Slides';
 import './style.css';
-import './styles/app.scss';
 
+// TODO : Create several pages with links and router
 render(<Intro/>, document.getElementById('root'));
 render(<Search/>, document.getElementById('search'));
 render(<Slides/>, document.getElementById('slideshow'));
