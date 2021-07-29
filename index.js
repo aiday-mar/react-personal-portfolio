f9484bc2c6ce6bf7b85c75e0ebf0cdb8d07f7f0d
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-07-29 12:30:22 +0200

    Navigation bar implemented

--- index.js
+++ index.js
@@ -4,39 +4,48 @@ import Main from './Components/Main/Main';
 import Posts from './Components/Posts/Posts';
 import Projects from './Components/Projects/Projects';
 import {
+  NavLink,
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+
+import NavBar from "./navBar.js";
+import "./style.css"
 
 function Routing() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/posts">Posts</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/projects" component={Projects} />
-          <Route path="/posts" component={Posts} />
-          <Route exact path="/" component={Main} />
-        </Switch>
+    <div className="App">
+      <div className="Body">
+        <Router>
+          <div>
+            <NavBar/>
+            <Switch>
+              <Route path="/projects" component={Projects} />
+              <Route path="/posts" component={Posts} />
+              <Route exact path="/" component={Main} />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    </Router>
+    </div> 
   );
 }
 
 render(<Routing/>, document.getElementById('routing'))
 
+/*
+<nav class="navigation">
+          <ul>
+            <li>
+              <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/posts">Posts</NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/projects">Projects</NavLink>
+            </li>
+          </ul>
+        </nav>
+        */
---
+++ navBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="NavBar">
+        <ul id="navigation-ul">
+          <li id="navigation-li">
+            <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/">Home</NavLink>
+          </li>
+          <li id="navigation-li">
+            <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/posts">Posts</NavLink>
+          </li>
+          <li id="navigation-li">
+            <NavLink activeStyle={{ color:'#000000' }} style={{ color: '#000000' }} to="/projects">Projects</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
