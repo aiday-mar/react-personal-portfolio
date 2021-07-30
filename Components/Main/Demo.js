0cee80ebd1e4c046bee594586a943b8d6db0195d
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-07-30 18:09:08 +0200

    Implemented card within grow, now I need to clean the page

--- Components/Main/Demo.js
+++ Components/Main/Demo.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Switch from '@material-ui/core/Switch';
-import Paper from '@material-ui/core/Paper';
 import Grow from '@material-ui/core/Grow';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: 220,
+    height: 1/3,
   },
   container: {
     display: 'flex',
   },
   paper: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1.2),
   },
   svg: {
     width: 200,
@@ -26,39 +31,111 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// --- CARD
+
+const faces = [
+  "http://i.pravatar.cc/300?img=1",
+  "http://i.pravatar.cc/300?img=2",
+  "http://i.pravatar.cc/300?img=3",
+  "http://i.pravatar.cc/300?img=4"
+];
+
+const styles = muiBaseTheme => ({
+  card: {
+    maxWidth: 300,
+    margin: "auto",
+    transition: "0.3s",
+    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+    "&:hover": {
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+    }
+  },
+  media: {
+    paddingTop: "56.25%"
+  },
+  content: {
+    textAlign: "left",
+    padding: muiBaseTheme.spacing.unit * 3
+  },
+  divider: {
+    margin: `${muiBaseTheme.spacing.unit * 3}px 0`
+  },
+  heading: {
+    fontWeight: "bold"
+  },
+  subheading: {
+    lineHeight: 1.8
+  },
+  avatar: {
+    display: "inline-block",
+    border: "2px solid white",
+    "&:not(:first-of-type)": {
+      marginLeft: -muiBaseTheme.spacing.unit
+    }
+  }
+});
+
+
+function App({ classes }) {
+  return (
+    <div className="App">
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={
+            "https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg"
+          }
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            className={"MuiTypography--heading"}
+            variant={"h6"}
+            gutterBottom
+          >
+            Nature Around Us
+          </Typography>
+          <Typography
+            className={"MuiTypography--subheading"}
+            variant={"caption"}
+          >
+            We are going to learn different kinds of species in nature that live
+            together to form amazing environment.
+          </Typography>
+          <Divider className={classes.divider} light />
+          {faces.map(face => (
+            <Avatar className={classes.avatar} key={face} src={face} />
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+const WrappedApp = withStyles(styles)(App);
+// ---
+
 export default function SimpleGrow() {
   const classes = useStyles();
-  const [checked, setChecked] = React.useState(true);
+  const [checked, setchecked] = React.useState(true);
 
   return (
+    // the root sub description in the classes variable 
     <div className={classes.root}>
       <div className={classes.container}>
         <Grow in={checked}>
-          <Paper elevation={4} className={classes.paper}>
-            <svg className={classes.svg}>
-              <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
-            </svg>
-          </Paper>
+          <WrappedApp/>
         </Grow>
         <Grow
           in={checked}
           style={{ transformOrigin: '0 0 0' }}
           {...(checked ? { timeout: 1000 } : {})}>
-          <Paper elevation={4} className={classes.paper}>
-            <svg className={classes.svg}>
-              <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
-            </svg>
-          </Paper>
+          <WrappedApp/>
         </Grow>
         <Grow
           in={checked}
           style={{ transformOrigin: '0 0 0' }}
           {...(checked ? { timeout: 2000 } : {})}>
-          <Paper elevation={4} className={classes.paper}>
-            <svg className={classes.svg}>
-              <polygon points="0,100 50,00, 100,100" className={classes.polygon} />
-            </svg>
-          </Paper>
+          <WrappedApp/>
         </Grow>
       </div>
     </div>
