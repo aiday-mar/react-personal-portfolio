e56874c879e37c49b46f9f696d6491dd67122d4e
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-08-01 11:38:41 +0200

    Put the search bar in the projects page, need to make it prettier, and more like a dropdown list instead

--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Search from './Search'; 
 
 // export default class Projects extends React.Component
 export default function Projects () {
 
   return(
-    <h2> hello projects! </h2>
+    <Search/>
   );
 }
