563adef0e9951f941ded94e90fb1f76bdb4b5f3d
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-08-01 13:35:58 +0200

    1 file modified

--- Components/Main/Slideshow.js
+++ Components/Main/Slideshow.js
@@ -4,17 +4,24 @@ const colors = ["#0088FE", "#00C49F", "#FFBB28"];
 const delay = 5000;
 
 export default function Slideshow() {
+  // variable which is available for the duration of the component
   const [index, setIndex] = React.useState(0);
   const timeoutRef = React.useRef(null);
 
   function resetTimeout() {
+    // if the value is not null
     if (timeoutRef.current) {
+      // cancels a timeout previously established by calling setTimeout
       clearTimeout(timeoutRef.current);
     }
   }
 
+  // after dom created we return from the first slide 
   React.useEffect(() => {
     resetTimeout();
+    // access the current value of the timeout 
+    // supposing we have 3 colors and hence we have prevIndex now 2,
+    // then we need to move to the zero index, otherwise to the next index
     timeoutRef.current = setTimeout(
       () =>
         setIndex((prevIndex) =>
@@ -30,10 +37,7 @@ export default function Slideshow() {
 
   return (
     <div className="slideshow">
-      <div
-        className="slideshowSlider"
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
+      <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
         {colors.map((backgroundColor, index) => (
           <div
             className="slide"
@@ -42,12 +46,9 @@ export default function Slideshow() {
           ></div>
         ))}
       </div>
-
-      <div className="slideshowDots">
-        {colors.map((_, idx) => (
-          <div
-            key={idx}
-            className={`slideshowDot${index === idx ? " active" : ""}`}
+      
+      <div className="slideshowDots"> {colors.map((_, idx) => (
+          <div key={idx} className={`slideshowDot${index === idx ? " active" : ""}`}
             onClick={() => {
               setIndex(idx);
             }}
