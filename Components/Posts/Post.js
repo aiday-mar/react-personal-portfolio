19c60a49fb2830a993ba3e668303c35d6ade9c25
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-08-09 20:05:10 +0200

    modified the style of the posts

--- Components/Posts/Post.js
+++ Components/Posts/Post.js
@@ -1,14 +1,17 @@
 import {useLocation} from 'react-router-dom'
 import React from 'react';
 import {useState,useEffect} from 'react';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap');
+</style>
 
 export default function Post(){
   let location = useLocation();
   const { postId, postTitle, postContent } = location.state
   return (
     <div>
-      <div style={{textAlign: "center", marginTop:20, fontSize : 30, color: "#787a7d"}}>{postTitle}</div>,
-      <div dangerouslySetInnerHTML={ { __html: postContent } }></div>
+      <div style={{textAlign: "center", marginTop:25, fontSize : 20, color: "#505e5a"}}>{postTitle}</div>,
+      <div dangerouslySetInnerHTML={{ __html: postContent }} className = "container-article" style={{marginTop : 20}}></div>
     </div>
   );
 }
