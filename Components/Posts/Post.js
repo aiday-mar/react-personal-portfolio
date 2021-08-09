203be0a206f27c1d5a4149561e114982498f96bf
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-08-09 20:53:46 +0200

    added disqus to the site, so now people can comment below the article

--- Components/Posts/Post.js
+++ Components/Posts/Post.js
@@ -1,17 +1,31 @@
 import {useLocation} from 'react-router-dom'
 import React from 'react';
 import {useState,useEffect} from 'react';
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap');
-</style>
+import {DiscussionEmbed} from "disqus-react"
+
 
 export default function Post(){
   let location = useLocation();
   const { postId, postTitle, postContent } = location.state
+
+  const disqusShortname = "aiday"
+  const disqusConfig = {
+    url: window.location.href,
+    identifier: postId, // Single post id
+    title: postTitle // Single post title
+  }
+
   return (
     <div>
       <div style={{textAlign: "center", marginTop:25, fontSize : 20, color: "#505e5a"}}>{postTitle}</div>,
-      <div dangerouslySetInnerHTML={{ __html: postContent }} className = "container-article" style={{marginTop : 20}}></div>
+      <div dangerouslySetInnerHTML={{ __html: postContent }} className = "container-article" 
+      style={{marginTop : 20}}></div>
+      <div style={{margin : "auto", justifyContent : "center", width : 50 + '%', marginTop : 100, borderTop: "solid 1px #000000"}}>
+      <DiscussionEmbed 
+        shortname={disqusShortname}
+        config={disqusConfig}
+      />
+      </div>
     </div>
   );
 }
