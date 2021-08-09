34d95adfa1a895efef5a470ea37a8a08b81073b2
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-08-09 09:52:27 +0200

    it's displaying only the last post in firebase, make it display all of them, and make link open up article in a separate tab

--- Components/Posts/Posts.js
+++ Components/Posts/Posts.js
@@ -1,29 +1,10 @@
 import React from 'react';
-// import {firestore, storage} from "./../../firebaseConfig.js";
+import {firestore, storage} from "./../../firebaseConfig.js";
 import React,{useState,useEffect} from 'react';
 import firebase from "firebase/app";
 import "firebase/firestore";
 
 export default function Posts() {
-
-    
-    const firebaseConfig = {
-    apiKey: "AIzaSyCt881Whr_wRdjoiXMaYT9tDVWoFMjM8Dk",
-    authDomain: "aiday-marlen-kyzy.firebaseapp.com",
-    databaseURL: "https://aiday-marlen-kyzy.firebaseio.com",
-    projectId: "aiday-marlen-kyzy",
-    storageBucket: "aiday-marlen-kyzy.appspot.com",
-    messagingSenderId: "215890159247",
-    appId: "1:215890159247:web:6c6e0a7e5262a3e57372fa",
-    measurementId: "G-X55KFKXFK7"
-    };
-
-    if (!firebase.apps.length) {
-        const firebaseApp = firebase.initializeApp(firebaseConfig);
-    }
-
-    const firestore = firebase.firestore();
-    firestore.enableLogging(true);
 
     const [blogs,setBlogs]=useState([])
     const fetchBlogs=async()=>{
@@ -36,6 +17,7 @@ export default function Posts() {
     useEffect(() => {
       fetchBlogs();
     }, [])
+    
     return (
       <div>
         {
