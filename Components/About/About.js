63f4a2bf97572e2fb3c93b7a62c52d86d1929785
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-02 10:29:20 +0100

    changed font in the contacts page to calibri

--- Components/About/About.js
+++ Components/About/About.js
@@ -5,7 +5,7 @@ export default class About extends React.Component {
   
   render() {
     return(
-      <div className="container-article" style={{marginTop:30}}>
+      <div className="container-article" style={{marginTop:30, fontFamily:'calibri'}}>
         GitHub : <a href="https://github.com/aiday-mar?tab=repositories"> https://github.com/aiday-mar?tab=repositories </a>
         <br/>
         LinkedIn : <a href="https://www.linkedin.com/in/aiday-marlen-kyzy/"> https://www.linkedin.com/in/aiday-marlen-kyzy/ </a>
