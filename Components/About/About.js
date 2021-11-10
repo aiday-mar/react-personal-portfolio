5c928362728c32c5f88c4ef21fe388f3a5945fce
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-10 15:45:45 +0100

    Made the navbarlink transition time 0.01 seconds, so it quickly transitions, and changed the contact page

--- Components/About/About.js
+++ Components/About/About.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import "./../../style.css"
+import './../../style.css';
 
 export default class About extends React.Component {
-  
   render() {
-    return(
-      <div className="container-article" style={{marginTop:30, fontFamily:'Arial Narrow'}}>
-        GitHub : <a href="https://github.com/aiday-mar?tab=repositories"> https://github.com/aiday-mar?tab=repositories </a>
-        <br/>
-        LinkedIn : <a href="https://www.linkedin.com/in/aiday-marlen-kyzy/"> https://www.linkedin.com/in/aiday-marlen-kyzy/ </a>
-        <br/>
+    return (
+      <div
+        className="container-article"
+        style={{
+          marginTop: 30,
+          fontFamily: 'Arial Narrow',
+          display: 'block',
+        }}
+      >
+        <a href="https://github.com/aiday-mar?tab=repositories" className="fa fa-github" style={{fontSize: 25,color: 'black', textDecoration:'none'}}></a> 
+        <a href="https://github.com/aiday-mar?tab=repositories" style={{marginLeft:'20px'}}>
+          https://github.com/aiday-mar?tab=repositories
+        </a>
+        <br />
+        <br />
+        <a href="https://www.linkedin.com/in/aiday-marlen-kyzy/" className="fa fa-linkedin" style={{fontSize: 25,color: 'black',  textDecoration:'none'}}></a>
+        <a href="https://www.linkedin.com/in/aiday-marlen-kyzy/" style={{marginLeft:'20px'}}>
+          https://www.linkedin.com/in/aiday-marlen-kyzy/
+        </a>
+        <br />
+        <br />
         Location : Renens, Switzerland
       </div>
     );
