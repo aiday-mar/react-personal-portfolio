cc8c527495afe1fb4e6aa39f9522eabafaea2448
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-10 16:26:03 +0100

    Removed some unecessary comments, need to find how to add string to URL

--- Components/Main/Main.js
+++ Components/Main/Main.js
@@ -1,11 +1,7 @@
 import React from 'react';
-// import Search from './Search'; put in navbar and make a better search bar than what I have now
-// import Slides from './Slides'; Create better carousel
 import Demo from './Demo';
 import Slideshow from './Slideshow';
 
-// export default class Routing extends React.Component
-// export default function Main()
 export default class Main extends React.Component {
   render() {
     return [<Slideshow />, <Demo />];
