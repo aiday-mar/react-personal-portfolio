8012205a593cfb909e3e387b1398f1e415aa3a1d
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-10 16:18:45 +0100

    Put the project data in a different JS file

--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -1,33 +1,6 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-const posts = {
-  results: [
-    {
-      id: 1,
-      title: 'Using Spark to build a recommendation engine',
-      date: '13/09/2021',
-      body: '<div> <p>I need to render this article</p> </div>',
-      icon: 'https://logowik.com/content/uploads/images/scala3486.jpg',
-    },
-    {
-      id: 2,
-      title: 'Optimizing the poisson algorithm execution using CUDA in C++',
-      date: '13/09/2021',
-      body: 'Describe the project',
-      icon: 'https://jtes.net/wp-content/uploads/2015/05/NVIDIA_CUDA_V_2C_r.jpg',
-    },
-    {
-      id: 3,
-      title:
-        'Creating a neural network to classify numbers using Keras in Python',
-      date: '13/09/2021',
-      body: 'Talk about the project for EPFL',
-      icon: 'https://www.logiciels.pro/wp-content/uploads/2021/05/keras-avis-prix-alternatives-logiciel.webp',
-    },
-  ],
-};
+import { posts } from './projectsData.js';
 
 export default function Projects() {
   return (
